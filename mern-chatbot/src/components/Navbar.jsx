3d608ef8669f027129de4079019855793cc57eb5
kamrancompany <132373098+kamrancompany@users.noreply.github.com>
2024-05-02 22:04:05 +0500

    modify:mernchatbot/src/components/Navbar.jsx  modify styling of file

--- mern-chatbot/src/components/Navbar.jsx
+++ mern-chatbot/src/components/Navbar.jsx
@@ -1,35 +1,56 @@
 import React from "react";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import "../style/Navbar.css";
-import logo from '../assets/chatbotImages/bot_images.png'
+import logo from "../assets/chatbotImages/bot_images.png";
 
 const Navbar = () => {
   const auth = localStorage.getItem("users");
   const navigate = useNavigate();
 
-  const logout =()=>{
+  const logout = () => {
     localStorage.clear();
-    navigate("/signin")
-  }
+    navigate("/signin");
+  };
 
   return (
     <>
       <div className="navbar_main_div">
-        <img src={logo} style={{height:"50px",width:"50px",borderRadius:"15px"}}/>
-      {auth ? (<>
-        <Link to="/" className="navbar-links">
-          Bard Ai
-        </Link>
-        <Link to="/auto" className="navbar-links">
-          ChatBot
-        </Link>
-        
-          <Link onClick={logout} to="/signin" className="navbar-links">
-            Logout
-          </Link>
+        <img
+          src={logo}
+          style={{ height: "50px", width: "50px", borderRadius: "15px" }}
+        />
+        {auth ? (
+          <>
+            <Link
+              to="/"
+              className="navbar-links"
+              style={{ fontSize: "20px", fontWeight: "bold" }}
+            >
+              Bard Ai
+            </Link>
+            <Link
+              to="/auto"
+              className="navbar-links"
+              style={{ fontSize: "20px", fontWeight: "bold" }}
+            >
+              ChatBot
+            </Link>
+
+            <Link
+              onClick={logout}
+              to="/signin"
+              className="navbar-links"
+              style={{ fontSize: "20px", fontWeight: "bold" }}
+            >
+              Logout
+            </Link>
           </>
         ) : (
-          <Link to="/signin" className="navbar-links">
+          <Link
+            to="/signin"
+            className="navbar-links"
+            style={{ fontSize: "20px", fontWeight: "bold" }}
+          >
             SignIn
           </Link>
         )}
